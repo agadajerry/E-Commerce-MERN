eb6cf99d32b1d91f3874adbedb1bd533d0ae2fcc
Jerrysoftechy.com <mac@Jerrysofttechycom.local>
2022-01-07 03:46:43 +0100

    Product category update

--- src/client/components/ProductCategory.jsx
+++ src/client/components/ProductCategory.jsx
@@ -5,8 +5,8 @@ function ProductCategory() {
             <div className="container mt-5 ms-3">
              
                 <div className="row">
-                     <div className="col-md-4"></div>
-                     <div className="col-md-4"></div>
+                     <div className="col-md-4 col-sm-4"></div>
+                     <div className="col-md-4 col-sm-4"></div>
                     <div className="col-md-4 d-flex  navbar-light py-2">
                         <a href ="/allproduct" className="ms-3 btn btn-secondary">All Products</a>
                         <a href ="/WomenWears/female" className="ms-3 btn btn-secondary ">Women's Wear</a>
